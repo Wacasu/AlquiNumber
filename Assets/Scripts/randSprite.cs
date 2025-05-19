7490d71232bbc41aded9948c501a8edb604060b3
sigmavoid <axel.ldpz@gmail.com>
2025-05-18 19:29:07 -0600

    *randomizer de items

--- Assets/Scripts/randSprite.cs
+++ Assets/Scripts/randSprite.cs
@@ -1,20 +1,22 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 
 public class randSprite : MonoBehaviour
 
 {
-    public Sprite[] sprites;
-
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] Image image;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        
-        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        float f = Random.Range(0, sprites.Length);
+        image.sprite = sprites[(int)f];
+
+        //GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
     }
 
     // Update is called once per frame
